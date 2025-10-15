7e82d3b2296a826871fbc632f8e51aa2e9ab6122
Donny Wilson <heydwilson@gmail.com>
2025-10-15 12:25:05 -0400

    Prep for  railway

--- application.py
+++ application.py
@@ -1,4 +1,11 @@
 from src.app import app as application
+from flask_cors import CORS
 
 if __name__ == '__main__':
     application.run()
+
+CORS(application, resources={r"/*": {"origins": "*"}})
+
+@application.route("/healthz")
+def healthz():
+    return {"status": "ok"}
